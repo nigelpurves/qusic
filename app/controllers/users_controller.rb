cbe7198fa1ad6190b21ef1a3978b9fc28c51e63d
Nigel Purves <nigelpurves@Nigel-Purvess-MacBook-Pro.local>
2012-05-22 11:30:34 +0100

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,21 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
     @title = "Sign up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    # When certain signup info is put in, the above is exactly equal to: 
+    # @user = User.new(:name => "Foo Bar", :email => "foo@invalid", :password => "dude", :password_confirmation => "dude")
+    
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user   # sends the user who successfully signs up to their profile page
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 end
