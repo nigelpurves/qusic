4f5e369e5c7811cee5d8f82eb0f6af0e707ae86b
Nigel Purves <nigelpurves@Nigel-Purvess-MacBook-Pro.local>
2012-05-23 08:09:51 +0100

    Done with sign in & exercises

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,8 @@
 module SessionsHelper
   
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    # cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:user_id] = user.id
     current_user = user
   end
   
@@ -10,7 +11,8 @@ module SessionsHelper
   end
   
   def current_user
-    @current_user ||= user_from_remember_token          # the effect of '||=' ("or equals") assignment operator is to set the @current_user instance variable to the user 
+    @current_user ||=User.find(session[:user_id]) if session[:user_id]
+    # @current_user ||= user_from_remember_token          # the effect of '||=' ("or equals") assignment operator is to set the @current_user instance variable to the user 
   end                                                   # corresponding to the remember token, but only if @current_user is undefined. In other words the whole line calls
                                                         # the user_from_remember_token method the first time current_user is called, but on subsequent invocations returns
                                                         # current user WITHOUT calling from user_from_remember_token
@@ -20,18 +22,19 @@ module SessionsHelper
   end
   
   def sign_out
-    cookies.delete(:remember_token)                     # effectively undoes the sign_in method by deleting the remember token and by setting the current user to nil
+    # cookies.delete(:remember_token)                     # effectively undoes the sign_in method by deleting the remember token and by setting the current user to nil
+    session[:user_id] = nil
     current_user = nil
   end
   
   private
   
-    def user_from_remember_token
-      User.authenticate_with_salt(*remember_token)
-    end
+  #  def user_from_remember_token
+  #    User.authenticate_with_salt(*remember_token)
+  #  end
     
-    def remember_token
-      cookies.signed[:remember_token] || [nil, nil]     # support for signed cookies in Rails is immature and a nil value for the cookie causes spurious test breakage...
-    end                                                 #...returning [nil, nil] fixes the issue
+  #  def remember_token
+  #    cookies.signed[:remember_token] || [nil, nil]     # support for signed cookies in Rails is immature and a nil value for the cookie causes spurious test breakage...
+  #  end                                                 #...returning [nil, nil] fixes the issue
   
 end
